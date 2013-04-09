5bda7c1d7dbcfbef431bec2305119afb0e51fb95
zhongsheng <passerbycrk@gmail.com>
2013-04-09 10:37:58 +0800

    first commit

---
+++ PKVideoPlayer/PKViewController.h
@@ -0,0 +1,13 @@
+//
+//  PKViewController.h
+//  PKVideoPlayer
+//
+//  Created by zhongsheng on 13-4-9.
+//  Copyright (c) 2013年 icePhone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PKViewController : UIViewController
+
+@end
---
+++ PKVideoPlayer/PKViewController.m
@@ -0,0 +1,29 @@
+//
+//  PKViewController.m
+//  PKVideoPlayer
+//
+//  Created by zhongsheng on 13-4-9.
+//  Copyright (c) 2013年 icePhone. All rights reserved.
+//
+
+#import "PKViewController.h"
+
+@interface PKViewController ()
+
+@end
+
+@implementation PKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
